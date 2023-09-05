3cc4007fad894b5162c4802d45877d2bbe85e530
Samuel A. Sutherland <Samuel.A.Sutherland@warwick.ac.uk>
2023-09-05 10:38:09 +0100

    Init repo

---
+++ make.R
@@ -0,0 +1,31 @@
+#' Rcompendium-learning: A Research Compendium
+#' 
+#' @description 
+#' A paragraph providing a full description of the project and describing each 
+#' step of the workflow.
+#' 
+#' @author Samuel Sutherland \email{Samuel.A.Sutherland@warwick.ac.uk}
+#' 
+#' @date 2023/09/05
+
+
+
+## Install Dependencies (listed in DESCRIPTION) ----
+
+devtools::install_deps(upgrade = "never")
+
+
+## Load Project Addins (R Functions and Packages) ----
+
+devtools::load_all(here::here())
+
+
+## Global Variables ----
+
+# You can list global variables here (or in a separate R script)
+
+
+## Run Project ----
+
+# List all R scripts in a sequential order and using the following form:
+# source(here::here("analyses", "script_X.R"))
